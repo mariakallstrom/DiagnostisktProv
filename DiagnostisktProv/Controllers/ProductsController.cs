829c3b65dc4205b1cb7368d4aa588089c3d6866d
mariakallstrom <mariakallstrom@outlook.com>
2017-09-07 11:31:28 +0200

    DONE

--- DiagnostisktProv/Controllers/ProductsController.cs
+++ DiagnostisktProv/Controllers/ProductsController.cs
@@ -94,8 +94,7 @@ namespace DiagnostisktProv.Controllers
         }
 
         // POST: Products/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,Price, ProductCategoryId")] Product product)
