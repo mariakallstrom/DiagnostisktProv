bbc43bf079623a86235fec7ec508caa3e194ea6c
mariakallstrom <mariakallstrom@outlook.com>
2017-09-07 09:48:49 +0200

    Added Validation on ProductModel

--- DiagnostisktProv/Models/Product.cs
+++ DiagnostisktProv/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,9 @@ namespace DiagnostisktProv.Models
     {
 
         public int ProductId { get; set; }
-
+        [Required]
         public string ProductName { get; set; }
-
+        [Range(5,1000)]
         public decimal Price { get; set; }
     }
 }
