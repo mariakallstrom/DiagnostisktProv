ade6133b660acb16e15a1cb4b10c52cce79d7cfb
mariakallstrom <mariakallstrom@outlook.com>
2017-09-07 09:45:56 +0200

    Added ProductModel

---
+++ DiagnostisktProv/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiagnostisktProv.Models
+{
+    public class Product
+    {
+
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
